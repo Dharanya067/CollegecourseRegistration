6be8e0a853a97753b6b68e6fa3d109b2e81242da
Dharanya067 <dharanya.240073@cse.ritchennai.edu.in>
2025-11-14 10:01:16 +0530

    Delete CollegeCourseRegistration_Backend_Package (1) directory

--- CollegeCourseRegistration_Backend_Package (1)/backend/src/main/java/com/attendance/repository/AttendanceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.attendance.repository;
-
-import com.attendance.model.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    List<Attendance> findByOrderByTimestampDesc();
-}
--- CollegeCourseRegistration_Backend_Package (1)/backend/src/main/java/com/attendance/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.attendance.repository;
-
-import com.attendance.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
